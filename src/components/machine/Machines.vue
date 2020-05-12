55708568ba53015074633c6954dff4b913ade253
nogodfyb <120626481@qq.com>
2020-05-12 15:11:45 +0800

    add machine module

---
+++ src/components/machine/Machines.vue
@@ -0,0 +1,45 @@
+<template>
+    <div>
+      <!-- 面包屑导航 -->
+      <el-breadcrumb separator-class="el-icon-arrow-right">
+        <el-breadcrumb-item :to="{ path: '/home' }">首页</el-breadcrumb-item>
+        <el-breadcrumb-item>机台管理</el-breadcrumb-item>
+        <el-breadcrumb-item>机台列表</el-breadcrumb-item>
+      </el-breadcrumb>
+      <el-card>
+        <!--        机台列表-->
+        <el-table :data="machineList" border stripe>
+          <el-table-column type="index"></el-table-column>
+          <el-table-column label="机台编号" prop="code"></el-table-column>
+          <el-table-column label="机台描述" prop="description"></el-table-column>
+        </el-table>
+      </el-card>
+    </div>
+</template>
+
+<script>
+export default {
+  created () {
+    this.getMachineList()
+  },
+  data () {
+    return {
+      machineList: []
+    }
+  },
+  methods: {
+    // 获取权限列表
+    async getMachineList () {
+      const { data: res } = await this.$http.get('machine/list')
+      if (res.status !== 200) {
+        return this.$message.error('获取权限列表失败！')
+      }
+      this.machineList = res.data
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -6,6 +6,7 @@ import Welcome from '../components/Welcome.vue'
 import Users from '../components/user/users.vue'
 import Rights from '../components/power/Rights.vue'
 import Roles from '../components/power/Roles.vue'
+import Machines from '../components/machine/Machines.vue'
 
 Vue.use(VueRouter)
 
@@ -20,7 +21,8 @@ const routes = [
       { path: '/welcome', component: Welcome },
       { path: '/users', component: Users },
       { path: '/rights', component: Rights },
-      { path: '/roles', component: Roles }
+      { path: '/roles', component: Roles },
+      { path: '/machines', component: Machines }
     ]
   }
 ]
