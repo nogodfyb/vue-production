cc3a4930d83b7649dc6a3d0fb8fb6207e782e458
nogodfyb <120626481@qq.com>
2020-05-16 23:40:13 +0800

    add main-dev.js main-prod.js

--- src/main-dev.js
+++ src/main-dev.js
@@ -6,8 +6,7 @@ import './assets/css/global.css'
 import './assets/fonts/iconfont.css'
 
 import axios from 'axios'
-// axios.defaults.baseURL = 'http://localhost:8082/production/'
-axios.defaults.baseURL = 'http://112.74.51.188:8080/production/'
+axios.defaults.baseURL = 'http://localhost:8082/production/'
 // 允许携带cookie
 axios.defaults.withCredentials = true
 Vue.prototype.$http = axios
