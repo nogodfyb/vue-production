913276ecd10c0666fc755fc9fcf6d83ac9fdce6f
nogodfyb <120626481@qq.com>
2020-05-17 09:15:00 +0800

    add external cdn

--- vue.config.js
+++ vue.config.js
@@ -1,8 +1,15 @@
 module.exports = {
   chainWebpack: config => {
+    // 生产环境
     config.when(process.env.NODE_ENV === 'production', config => {
       config.entry('app').clear().add('./src/main-prod.js')
+      config.set('externals', {
+        vue: 'Vue',
+        'vue-router': 'VueRouter',
+        axios: 'axios'
+      })
     })
+    // 开发环境
     config.when(process.env.NODE_ENV === 'development', config => {
       config.entry('app').clear().add('./src/main-dev.js')
     })
